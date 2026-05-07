aa24423f4af0443f726874145f601809d5465ad3
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-05-07 23:40:41 +0630

    Add initial Next.js configuration file

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
