cd1aa328411eff8315888f3566745cbad17fbcd6
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-05-07 23:39:46 +0630

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          950: '#030712',
+        },
+      },
+      animation: {
+        float: 'float 3s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
